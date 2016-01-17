cfbb8b3652c3a387e90362befc43a833ff3f092b
Tasdik Rahman <tasdik95@gmail.com>
2016-01-17 14:40:36 +0530

    added mob elements and their movements. To add collisions

--- spaceShooter.py
+++ spaceShooter.py
@@ -1,9 +1,17 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: tasdik
+# @Date:   2016-01-17
+# @Email:  prodicus@outlook.com
+# @Last Modified by:   tasdik
+# @Last Modified time: 2016-01-17
+# MIT License. You can find a copy of the License @ http://prodicus.mit-license.org
 
 import pygame
 import random
 
-
+###############################
+## placed in "constant.py"
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
@@ -14,13 +22,17 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+###############################
 
+###############################
+## placed in "__init__.py"
 ## initialize pygame and create window
 pygame.init()
 pygame.mixer.init()  ## For sound
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter")
 clock = pygame.time.Clock()     ## For syncing the FPS
+###############################
 
 
 class Player(pygame.sprite.Sprite):
@@ -38,7 +50,8 @@ class Player(pygame.sprite.Sprite):
         # then we have to check whether there is an event hanlding being done for the arrow keys being 
         ## pressed 
 
-        keystate = pygame.key.get_pressed()     ## will give back a list of the keys which happen to be pressed down at that moment
+        ## will give back a list of the keys which happen to be pressed down at that moment
+        keystate = pygame.key.get_pressed()     
         if keystate[pygame.K_LEFT]:
             self.speedx = -5
         if keystate[pygame.K_RIGHT]:
@@ -53,11 +66,42 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += self.speedx
 
 
+# defines the enemies
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)        ## for randomizing the speed of the Mob
+
+        ## randomize the movements a little more 
+        self.speedx = random.randrange(-3, 3)
+        
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        ## now what if the mob element goes out of the screen
+
+        if (self.rect.top > HEIGHT + 10) or (self.rect.left < -25) or (self.rect.right > WIDTH + 20):
+            self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)        ## for randomizing the speed of the Mob
+            
+
 ## group all the sprites together for ease of update
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
 
+## spawn a group of mob
+mobs = pygame.sprite.Group()
+for i in range(8):      ## 8 mobs
+    mob_element = Mob()
+    all_sprites.add(mob_element)
+    mobs.add(mob_element)
 
 ## Game loop
 running = True
