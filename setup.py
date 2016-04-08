3742a2512711a18519a6300951e1b569451daed5
Tasdik Rahman <tasdik95@gmail.com>
2016-04-09 01:50:52 +0530

    cleanup: Directory restructuring

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
+pygame_py_file = os.path.join('spaceshooter', 'spaceShooter.py')
+
 ## The image and sound files are added manually into the zip file
 ## A fix for this would be released
 
@@ -18,5 +20,5 @@ setup(  name = "Space Shooter",
         version = "0.0.2",
         description = "classic retro game made using pygame",
         options = {"build_exe": build_exe_options},
-        executables = [Executable("spaceShooter.py", base=base)]
+        executables = [Executable(pygame_py_file, base=base)]
 )
