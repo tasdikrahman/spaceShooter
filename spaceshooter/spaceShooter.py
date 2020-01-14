9020710b68a19bded12bb3c8c327131338dd9861
CodeLongAndProsper90 <50145141+CodeLongAndProsper90@users.noreply.github.com>
2020-01-14 13:49:20 -0600

    Update spaceShooter.py

--- spaceshooter/spaceShooter.py
+++ spaceshooter/spaceShooter.py
@@ -19,28 +19,8 @@ from __future__ import division
 import pygame
 import random
 from os import path
+from constans import *
 
-## assets folder
-img_dir = path.join(path.dirname(__file__), 'assets')
-sound_folder = path.join(path.dirname(__file__), 'sounds')
-
-###############################
-## to be placed in "constant.py" later
-WIDTH = 480
-HEIGHT = 600
-FPS = 60
-POWERUP_TIME = 5000
-BAR_LENGTH = 100
-BAR_HEIGHT = 10
-
-# Define Colors 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-###############################
 
 ###############################
 ## to placed in "__init__.py" later
